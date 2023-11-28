e92af2a0794bbeb50b5664b0f0001ec2e5ba0c2e
Andrew Jordaan <andrew.jordaan@platform45.com>
2023-11-28 09:10:24 +0000

    refactor: formatting

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -11,53 +11,52 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Fake DB
-let todos = [
-];
+let todos = [];
 
-app.get('/todos', (req, res) => {
+app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const todo = todos.find(todo => todo.id === id);
+  const todo = todos.find((todo) => todo.id === id);
 
   if (todo) {
     res.json(todo);
   } else {
-    res.status(404).json({ message: 'Todo not found' });
+    res.status(404).json({ message: "Todo not found" });
   }
 });
 
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   const newTodo = req.body;
   newTodo.persisted = true;
   todos.push(newTodo);
 
-  console.log('Added New Todo ', newTodo)
+  console.log("Added New Todo ", newTodo);
 
   res.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const updatedTodo = req.body;
-  updatedTodo.persisted = true
-  const index = todos.findIndex(todo => todo.id === id);
+  updatedTodo.persisted = true;
+  const index = todos.findIndex((todo) => todo.id === id);
 
   if (index !== -1) {
     todos[index] = { ...todos[index], ...updatedTodo };
     res.json(todos[index]);
   } else {
-    res.status(404).json({ message: 'Todo not found' });
+    res.status(404).json({ message: "Todo not found" });
   }
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const todo = todos.find(todo => todo.id === id);
-  todo.deleted = true
-  res.json({ message: 'Todo deleted successfully' });
+  const todo = todos.find((todo) => todo.id === id);
+  todo.deleted = true;
+  res.json({ message: "Todo deleted successfully" });
 });
 
 app.listen(port, () => {
