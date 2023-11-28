145d38cb831a128c53dfb11bcd0130b00ae14141
Andrew Jordaan <andrew.jordaan@platform45.com>
2023-11-28 09:09:20 +0000

    add: make calls from client to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Todo } from "./types";
 import TodoList from "./todoList";
 import AddTodoItem from "./AddTodoItem";
+import ky from 'ky';
 
 function App() {
 
   const [todos, setTodos] = useState([] as Todo[])
 
-  const createTodo = (description: string) => {
+  const createTodo = async (description: string) => {
     console.log('todo created')
     console.log(description)
     const todo: Todo = { id: 'sdff', description: description, persisted: false, completed: false }
+
+    const json = await ky.post('http://localhost:3000/todos', { json: todo }).json();
+    console.log(json)
     setTodos([todo, ...todos])
   }
+
+  const loadTodos = async() => {
+    const serverTodos: Todo[] = await ky.get('http://localhost:3000/todos').json();
+    console.log(serverTodos)
+    setTodos([...serverTodos])
+  }
+useEffect(() => {
+  loadTodos()
+  }, []);
+
 
 
   return (
